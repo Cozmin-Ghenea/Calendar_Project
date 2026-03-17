e0e185ee4e50e1e83a46c9f726634d2b06329286
Cozmin Ghenea <Cozmin.Ghenea@exesoftware.ro>
2026-03-17 10:31:49 +0200

    Update base URL

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/CalendarProject/', // Required for GitHub Pages deployment
+  base: '/Calendar_Project/', // Required for GitHub Pages deployment
 })
